faf6597e518702dee294b182e86b0c8c98c3187f
tadashi-aikawa <syou.maman@gmail.com>
2018-06-03 18:44:30 +0900

    :new: Add links for windows

--- provision.bat
+++ provision.bat
@@ -1,5 +1,7 @@
 @echo off
 
+set UBUNTU_MNT=%USERPROFILE%\Dropbox\ubuntu-mnt
+set WINDOWS_MNT=%USERPROFILE%\Dropbox\windows-mnt
 
 rem ------------------
 rem Install
@@ -8,15 +10,13 @@ rem ------------------
 cinst /y .\packages.config
 
 rem ------------------
-rem Sym links
+rem IntelliJ IDEA
 rem ------------------
 
-set MNT_DIR=%USERPROFILE%\Dropbox\ubuntu-mnt
-set IDEA_ORIGIN_CONFIG_DIR=%MNT_DIR%\.IntelliJIdea\config
-
 set IDEA_DIR=.IntelliJIdea2018.1
-set IDEA_CONFIG_DIR=%USERPROFILE%\%IDEA_DIR%\config
 
+set IDEA_ORIGIN_CONFIG_DIR=%UBUNTU_MNT%\.IntelliJIdea\config
+set IDEA_CONFIG_DIR=%USERPROFILE%\%IDEA_DIR%\config
 
 call :link_idea_dir colors
 call :link_idea_dir keymaps
@@ -27,24 +27,60 @@ call :link_idea_file options\keymap.xml
 call :link_idea_file options\markdown.xml
 call :link_idea_file options\vcs.xml
 call :link_idea_file options\vim_settings.xml
-call :link_file %USERPROFILE%\.ideavimrc %MNT_DIR%\.ideavimrc
+call :link_file %USERPROFILE%\.ideavimrc %UBUNTU_MNT%\.ideavimrc
+
+
+rem ------------------
+rem VS Code 
+rem ------------------
+
+set VSCODE_ORIGIN_USER_DIR=%UBUNTU_MNT%\.config\Code\User
+set VSCODE_USER_DIR=%USERPROFILE%\AppData\Roaming\Code\User
+
+call :link_vscode_file keybindings.json
+call :link_vscode_file settings.json
+call :link_vscode_dir snippets
+
+rem ------------------
+rem Homedir
+rem ------------------
+
+call :link_windows_home .bashrc
+call :link_windows_home .minttyrc
+call :link_windows_home .vimrc
 
 exit /b
 
 rem ---------------------------------------------------------
+
+:link_windows_home
+call :link_file %USERPROFILE%\%1 %WINDOWS_MNT%\%1
+exit /b
+
+:link_idea_file
+call :link_file %IDEA_CONFIG_DIR%\%1 %IDEA_ORIGIN_CONFIG_DIR%\%1
+exit /b
+
+:link_idea_dir
+call :link_dir %IDEA_CONFIG_DIR%\%1 %IDEA_ORIGIN_CONFIG_DIR%\%1
+exit /b
+
+:link_vscode_file
+call :link_file %VSCODE_USER_DIR%\%1 %VSCODE_ORIGIN_USER_DIR%\%1
+exit /b
+
+:link_vscode_dir
+call :link_dir %VSCODE_USER_DIR%\%1 %VSCODE_ORIGIN_USER_DIR%\%1
+exit /b
+
+rem ----- common ------
 
 :link_file
 del %1
 Mklink %1 %2
 exit /b
 
-:link_idea_file
-del %IDEA_CONFIG_DIR%\%1
-Mklink %IDEA_CONFIG_DIR%\%1 %IDEA_ORIGIN_CONFIG_DIR%\%1
+:link_dir
+rd /s /q %1
+Mklink /D %1 %2
 exit /b
-
-:link_idea_dir
-rd /s /q %IDEA_CONFIG_DIR%\%1
-Mklink /D %IDEA_CONFIG_DIR%\%1 %IDEA_ORIGIN_CONFIG_DIR%\%1
-exit /b
-
