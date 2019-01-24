7a9fda68f3cd228a1fd3c84b71ae4996a8f871c5
tadashi-aikawa <syou.maman@gmail.com>
2019-01-24 23:43:40 +0900

    テンプレとdeinの不要設定削除

--- mnt/linux/ubuntu/.vim-snippets/newtmpl/bash.sh
+++ mnt/linux/ubuntu/.vim-snippets/newtmpl/bash.sh
@@ -5,6 +5,16 @@ usage_exit() {
     exit 1
 }
 
+post_slack() {
+  local channel=$1
+  local user=$2
+  local emoji=$3
+  local message=$4
+  # good / danger
+  local color=$5
+  curl -X POST --data-urlencode 'payload={"link_names": 1, "channel": "'"${channel}"'", "username": "'"${user}"'", "icon_emoji": "'":${emoji}:"'", "attachments": [{"color": "'"${color}"'", "text": "'"${message}"'"}]}' ${SLACK_WEBHOOK_URL}
+}
+
 while getopts r:o:fh opt
 do
     case $opt in
