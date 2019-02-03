d3c828942954f7c8545c515e1442598d7b03d04b
tadashi-aikawa <syou.maman@gmail.com>
2019-02-03 21:11:43 +0900

    [Windows] aliasの順番整理

--- mnt/windows/cmder/config/user_aliases.cmd
+++ mnt/windows/cmder/config/user_aliases.cmd
@@ -4,7 +4,15 @@
 ;= rem In batch mode, jump to the end of the file
 ;= goto:eof
 ;= Add aliases below here
+
+
 e.=explorer .
+web="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" $*
+te="C:\tablacus\TE64.exe" $*
+cat="C:\tools\cmder\vendor\git-for-windows\usr\bin\cat.exe" $*
+history=cat "C:\tools\Cmder\config\.history" $*
+
+
 ls=ls --show-control-chars -F --color $*
 pwd=cd
 clear=cls
@@ -12,23 +20,23 @@ unalias=alias /d $1
 vi=vim $*
 cmderr=cd /d "%CMDER_ROOT%"
 ll=ls -l $*
-cd~=cd "%HOME%"  
-history=cat "C:\tools\Cmder\config\.history" $*  
-cat="C:\tools\cmder\vendor\git-for-windows\usr\bin\cat.exe" $*  
-rg=rg -L $*  
-gs=git status --short $*  
-gsv=git status -v $*  
-gd=git diff $*  
-gf=git fetch $*  
-gl=git log $*  
+cd~=cd "%HOME%"
+rg=rg -L $*
+
+
+gs=git status --short $*
+gsv=git status -v $*
+gd=git diff $*
+gf=git fetch $*
+gl=git log $*
 gll=git log --oneline --all --graph --decorate $*
 gls=git log -3 $*
 glll=git log --graph --all --date=format:"%Y-%m-%d %H:%M" --pretty=format:"%C(auto)%d%Creset %C(yellow reverse)%h%Creset %C(magenta)%ae%Creset %C(cyan)%ad%Creset%n%C(white bold)%w(80)%s%Creset%n%b"
 glls=git log --graph --all --date=format:"%Y-%m-%d %H:%M" --pretty=format:"%C(auto)%d%Creset %C(yellow reverse)%h%Creset %C(magenta)%ae%Creset %C(cyan)%ad%Creset%n%C(white bold)%w(80)%s%Creset%n%b" -10
-gcv=git commit -v $*  
-gcm=git commit -m $*  
-ga=git add $*  
-gaa=git add --all $*  
-~1=~0,-1  
-web="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" $*
-te="C:\tablacus\TE64.exe" $*  
+gcv=git commit -v $*
+gcm=git commit -m $*
+ga=git add $*
+gaa=git add --all $*
+
+
+~1=~0,-1
