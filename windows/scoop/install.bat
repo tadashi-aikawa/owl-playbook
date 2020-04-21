2820e05b2026cf2feb3858e09eb5fd260f4414d9
tadashi-aikawa <syou.maman@gmail.com>
2020-04-21 10:29:01 +0900

    [Windows] Chocolateyの移行可能なPackageをScoopに移行

--- windows/scoop/install.bat
+++ windows/scoop/install.bat
@@ -1,1 +1,38 @@
-@FOR /F "usebackq delims=:" %%t IN (`cat %~dp0packages.config ^| xargs`) DO call scoop install %%t
+rem Add versions bucket
+call scoop bucket add versions
+rem Add extras
+call scoop bucket add extras
+
+rem CLI Tools
+call scoop install ^
+  sudo ^
+  wget ^
+  bat ^
+  fd ^
+  7zip ^
+  fzf ^
+  jq ^
+  less ^
+  ripgrep ^
+  make ^
+  ffmpeg
+
+rem Language / Framework / MiddleWare
+call scoop install ^
+  nodejs-lts ^
+  docker ^
+  go ^
+  autohotkey ^
+  hugo-extended
+
+rem Python
+call scoop install ^
+  python27 ^
+  python37 ^
+  python
+call scoop reset python
+
+rem C Tools
+rem scoop install ^
+rem   llvm ^
+rem   cmake
