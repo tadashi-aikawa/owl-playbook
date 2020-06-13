71fbc234aa48194352aa07ece7080bbb31a734ea
tadashi-aikawa <syou.maman@gmail.com>
2020-06-14 01:14:32 +0900

    [WSL] ansible/pipのインストール/実行に失敗していたのを修正

--- windows/ubuntu/provision.sh
+++ windows/ubuntu/provision.sh
@@ -5,6 +5,5 @@ set -eu
 # Install ansible
 # https://docs.ansible.com/ansible/latest/user_guide/windows_faq.html#can-ansible-run-on-windows
 sudo apt-get update
-sudo apt-get install python-pip libffi-dev libssl-dev -y
-pip install ansible pywinrm
-
+sudo apt-get install python3-pip libffi-dev libssl-dev -y
+pip3 install ansible pywinrm
