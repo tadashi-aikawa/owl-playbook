de7e9edf6be53bd8a728e7272e02bc843f5ee57f
tadashi-aikawa <syou.maman@gmail.com>
2020-07-25 21:11:12 +0900

    [bash] Add MIRACLE TEHEME maman.theme.bash!!

--- mnt/linux/ubuntu/.bash_it/themes/maman/maman.theme.bash
+++ mnt/linux/ubuntu/.bash_it/themes/maman/maman.theme.bash
@@ -1,194 +1,239 @@
 #!/usr/bin/env bash
-# Inspired Power-Turk theme for bash-it
-# Author of Power-Turk (C) 2015 Ahmed Seref Guneysu
 
-THEME_PROMPT_SEPARATOR=""
-
-SHELL_SSH_CHAR=" "
-SHELL_THEME_PROMPT_COLOR=2
-SHELL_SSH_THEME_PROMPT_COLOR=208
-
-VIRTUALENV_CHAR="ⓔ "
-VIRTUALENV_THEME_PROMPT_COLOR=35
-
-SCM_NONE_CHAR=""
-
-SCM_GIT_CHAR=" " # " "
-SCM_GIT_CHAR_REMOTE_DEFAULT=" "
-SCM_GIT_CHAR_REMOTE_NO_DEFAULT=" "
-SCM_GIT_CHAR_GITHUB=" "
-SCM_GIT_CHAR_BITBUCKET=" "
+# Use in scm_prompt_vars
 SCM_GIT_STAGED_CHAR=" "
 SCM_GIT_UNSTAGED_CHAR=" "
 SCM_GIT_UNTRACKED_CHAR=" "
 SCM_GIT_AHEAD_CHAR=" "
 SCM_GIT_BEHIND_CHAR=" "
 SCM_THEME_TAG_PREFIX=" "
-
 SCM_THEME_PROMPT_CLEAN=""
 SCM_THEME_PROMPT_DIRTY=""
 
-SCM_THEME_PROMPT_COLOR=16
-SCM_THEME_PROMPT_CLEAN_COLOR=231
-SCM_THEME_PROMPT_DIRTY_COLOR=196
-SCM_THEME_PROMPT_STAGED_COLOR=220
-SCM_THEME_PROMPT_UNSTAGED_COLOR=166
+# Separator instead of /
+SEP=""
 
-CWD_THEME_PROMPT_COLOR=236 #240
-CWD_THEME_PROMPT_CHAR_COLOR=159
+##################################################
+#
+# | prompt > virtual_env > scm > cwd > status >
+#
+##################################################
+PROMPT_THEME_BG=2
 
-LAST_STATUS_THEME_PROMPT_COLOR=52
+# Python virtualenv
+VIRTUAL_ENV_FG=255
+VIRTUAL_ENV_BG=4
 
+VIRTUAL_ENV_SYMBOL=" "
 
-function _git-uptream-remote-logo {
-    if [[ "$(_git-upstream)" == "" ]]; then
-      SCM_GIT_CHAR_REMOTE="$SCM_GIT_CHAR_REMOTE_NO_DEFAULT"
-      return
-    fi
+# Source code management
+SCM_CLEAN_FG=231
+SCM_DIRTY_FG=196
+SCM_STAGED_FG=220
+SCM_UNSTAGED_FG=166
+SCM_BEHIND_FG=2
+SCM_AHEAD_FG=2
 
-    local remote_domain
-    remote_domain=$(git remote get-url origin | awk -F'[@:.]' '{print $2}')
-    case ${remote_domain//\//} in
-        github ) SCM_GIT_CHAR_REMOTE="$SCM_GIT_CHAR_GITHUB";;
-        bitbucket ) SCM_GIT_CHAR_REMOTE="$SCM_GIT_CHAR_BITBUCKET";;
-        * ) SCM_GIT_CHAR_REMOTE="$SCM_GIT_CHAR_REMOTE_DEFAULT";;
-    esac
+SCM_BG=16
+
+SCM_NONE_CHAR=""
+SCM_GIT_CHAR=" "
+SCM_GIT_CHAR_REMOTE_DEFAULT=" "
+SCM_GIT_CHAR_REMOTE_NO_DEFAULT=" "
+SCM_GIT_CHAR_GITHUB=" "
+SCM_GIT_CHAR_BITBUCKET=" "
+
+# Current working directory
+CWD_FG=159
+CWD_BG=236
+
+# Status
+STATUS_FG=1
+STATUS_BG=52
+
+#######################################
+# Arguments:
+#   None
+# Returns:
+#   None
+# Stdout:
+#   Logo emoji (ex: )
+#######################################
+function git_remote_logo() {
+  if [[ "$(_git-upstream)" == "" ]]; then
+    echo "$SCM_GIT_CHAR_REMOTE_NO_DEFAULT"
+    return
+  fi
+
+  local remote_domain
+  remote_domain=$(git remote get-url origin | awk -F'[@:.]' '{print $2}')
+  case ${remote_domain//\//} in
+    github) echo "$SCM_GIT_CHAR_GITHUB" ;;
+    bitbucket) echo "$SCM_GIT_CHAR_BITBUCKET" ;;
+    *) echo "$SCM_GIT_CHAR_REMOTE_DEFAULT" ;;
+  esac
 }
 
-_swd(){
-# Adapted from http://stackoverflow.com/a/2951707/1766716
-    begin="" # The unshortened beginning of the path.
-    shortbegin="" # The shortened beginning of the path.
-    current="" # The section of the path we're currently working on.
-    end="${2:-$(pwd)}/" # The unmodified rest of the path.
-
-    if [[ "$end" =~ "$HOME" ]]; then
-        INHOME=1
-        end="${end#$HOME}" #strip /home/username from start of string
-        begin="$HOME"      #start expansion from the right spot
-    else
-        INHOME=0
-    fi
-
-    end="${end#/}" # Strip the first /
-    shortenedpath="$end" # The whole path, to check the length.
-    maxlength="${1:-0}"
-
-    shopt -q nullglob && NGV="-s" || NGV="-u" # Store the value for later.
-    shopt -s nullglob    # Without this, anything that doesn't exist in the filesystem turns into */*/*/...
-
-    while [[ "$end" ]] && (( ${#shortenedpath} > maxlength ))
-    do
-        current="${end%%/*}" # everything before the first /
-        end="${end#*/}"    # everything after the first /
-
-        #advance
-        begin="$begin/$current"
-        shortbegin="$shortbegin/$current"
-        shortenedpath="$shortbegin/$end"
-    done
-
-    shortenedpath="${shortenedpath%/}" # strip trailing /
-    shortenedpath="${shortenedpath#/}" # strip leading /
-
-    # Replaces slashes with  except first occurence.
-    if [ $INHOME -eq 1 ]; then
-        echo "~/$shortenedpath " | sed "s/\//  /2g" # make sure it starts with ~/
-    else
-        echo "/$shortenedpath "  | sed "s/\//  /2g" # Make sure it starts with /
-    fi
-
-    shopt "$NGV" nullglob # Reset nullglob in case this is being used as a function.
-
-}
-function set_rgb_color {
-    if [[ "${1}" != "-" ]]; then
-        fg="38;5;${1}"
-    fi
-    if [[ "${2}" != "-" ]]; then
-        bg="48;5;${2}"
-        [[ -n "${fg}" ]] && bg=";${bg}"
-    fi
-    echo -e "\[\033[${fg}${bg}m\]"
+##############################################
+# Arguments:
+#   None
+# Returns:
+#   None
+# Stdout:
+#   Short current work directory (ex: ~/hoge)
+##############################################
+function short_cwd() {
+  echo "$(pwd) " |
+    sed -r "s@${HOME}/git/github.com@${SCM_GIT_CHAR_GITHUB}@g" |
+    sed -r "s@${HOME}@~@g" |
+    sed -r "s/\//  /2g"
 }
 
-function powerline_shell_prompt {
-    if [[ -n "${SSH_CLIENT}" ]]; then
-        SHELL_PROMPT="${bold_white}$(set_rgb_color - ${SHELL_SSH_THEME_PROMPT_COLOR}) ${SHELL_SSH_CHAR}\u@\h ${normal}"
-        LAST_THEME_COLOR=${SHELL_SSH_THEME_PROMPT_COLOR}
-    else
-        SHELL_PROMPT="${bold_white}$(set_rgb_color - ${SHELL_THEME_PROMPT_COLOR}) ${normal}"
-        LAST_THEME_COLOR=${SHELL_THEME_PROMPT_COLOR}
-    fi
+##############################################
+# Arguments:
+#   $1: Background color of separator
+# Stdout:
+#   Separator
+##############################################
+function separator() {
+  echo "$(bg "${1}")${SEP}"
 }
 
-function powerline_virtualenv_prompt {
-    local environ=""
-
-    if [[ -n "$CONDA_DEFAULT_ENV" ]]; then
-        environ="conda: $CONDA_DEFAULT_ENV"
-    elif [[ -n "$VIRTUAL_ENV" ]]; then
-        environ=$(basename "$VIRTUAL_ENV")
-    fi
-
-    if [[ -n "$environ" ]]; then
-        VIRTUALENV_PROMPT="$(set_rgb_color ${LAST_THEME_COLOR} ${VIRTUALENV_THEME_PROMPT_COLOR})${THEME_PROMPT_SEPARATOR}${normal}$(set_rgb_color - ${VIRTUALENV_THEME_PROMPT_COLOR}) ${VIRTUALENV_CHAR}$environ ${normal}"
-        LAST_THEME_COLOR=${VIRTUALENV_THEME_PROMPT_COLOR}
-    else
-        VIRTUALENV_PROMPT=""
-    fi
+##############################################
+# Arguments:
+#   $1: foreground color (ex: 208)
+# Returns:
+#   None
+# Stdout:
+#   Color string
+##############################################
+function fg() {
+  echo -e "\[\033[38;5;${1}m\]"
 }
 
-function powerline_scm_prompt {
-    scm_prompt_vars
-
-    if [[ "${SCM_NONE_CHAR}" != "${SCM_CHAR}" ]]; then
-        _git-uptream-remote-logo 
-
-        if [[ "${SCM_DIRTY}" -eq 3 ]]; then
-            SCM_PROMPT="$(set_rgb_color ${SCM_THEME_PROMPT_STAGED_COLOR} ${SCM_THEME_PROMPT_COLOR})"
-        elif [[ "${SCM_DIRTY}" -eq 2 ]]; then
-            SCM_PROMPT="$(set_rgb_color ${SCM_THEME_PROMPT_UNSTAGED_COLOR} ${SCM_THEME_PROMPT_COLOR})"
-        elif [[ "${SCM_DIRTY}" -eq 1 ]]; then
-            SCM_PROMPT="$(set_rgb_color ${SCM_THEME_PROMPT_DIRTY_COLOR} ${SCM_THEME_PROMPT_COLOR})"
-        else
-            SCM_PROMPT="$(set_rgb_color ${SCM_THEME_PROMPT_CLEAN_COLOR} ${SCM_THEME_PROMPT_COLOR})"
-        fi
-        if [[ "${SCM_GIT_CHAR}" == "${SCM_CHAR}" ]]; then
-            SCM_PROMPT+=" ${SCM_GIT_CHAR_REMOTE} ${SCM_CHAR}${SCM_BRANCH}${SCM_STATE}"
-        fi
-        
-        SCM_PROMPT="$(set_rgb_color ${LAST_THEME_COLOR} ${SCM_THEME_PROMPT_COLOR})${THEME_PROMPT_SEPARATOR}${normal}${SCM_PROMPT} ${normal}"
-        LAST_THEME_COLOR=${SCM_THEME_PROMPT_COLOR}
-    else
-        SCM_PROMPT=""
-    fi
+##############################################
+# Arguments:
+#   $1: background color (ex: 208)
+# Returns:
+#   None
+# Stdout:
+#   Color string
+##############################################
+function bg() {
+  echo -e "\[\033[48;5;${1}m\]"
 }
 
-function powerline_cwd_prompt {
-CWD_PROMPT="$(set_rgb_color ${LAST_THEME_COLOR} ${CWD_THEME_PROMPT_COLOR})${THEME_PROMPT_SEPARATOR}$(set_rgb_color ${CWD_THEME_PROMPT_CHAR_COLOR} ${CWD_THEME_PROMPT_COLOR}) $(_swd)${normal}$(set_rgb_color ${CWD_THEME_PROMPT_COLOR} -)${normal}"
-    LAST_THEME_COLOR=${CWD_THEME_PROMPT_COLOR}
+##############################################
+# Arguments:
+#   $1: foreground color (ex: 208)
+#   $2: background color (ex: 208)
+# Returns:
+#   None
+# Stdout:
+#   Color string
+##############################################
+function fgbg() {
+  echo -e "\[\033[38;5;${1};48;5;${2}m\]"
 }
 
-function powerline_last_status_prompt {
-    if [[ "$1" -eq 0 ]]; then
-        LAST_STATUS_PROMPT="$(set_rgb_color ${LAST_THEME_COLOR} -)${THEME_PROMPT_SEPARATOR}${normal}"
-    else
-        LAST_STATUS_PROMPT="$(set_rgb_color ${LAST_THEME_COLOR} ${LAST_STATUS_THEME_PROMPT_COLOR})${THEME_PROMPT_SEPARATOR}${normal}$(set_rgb_color - ${LAST_STATUS_THEME_PROMPT_COLOR}) ${LAST_STATUS} ${normal}$(set_rgb_color ${LAST_STATUS_THEME_PROMPT_COLOR} -)${THEME_PROMPT_SEPARATOR}${normal}"
-    fi
+##############################################
+# (1)
+##############################################
+function build_shell_prompt() {
+  echo "$(bg ${PROMPT_THEME_BG}) $(fg ${PROMPT_THEME_BG})"
 }
 
-function powerline_prompt_command() {
-    local LAST_STATUS="$?"
+##############################################
+# (2)
+##############################################
+function build_virtualenv_prompt() {
+  local environ=""
 
-    powerline_shell_prompt
-    powerline_virtualenv_prompt
-    powerline_scm_prompt
-    powerline_cwd_prompt
-    powerline_last_status_prompt LAST_STATUS
+  if [[ -n "$VIRTUAL_ENV" ]]; then
+    environ=$(basename "$VIRTUAL_ENV")
+  fi
 
-    PS1="${SHELL_PROMPT}${VIRTUALENV_PROMPT}${SCM_PROMPT}${CWD_PROMPT}${LAST_STATUS_PROMPT} "
+  if [[ -n "$environ" ]]; then
+    echo "$(separator ${VIRTUAL_ENV_BG}) $(fgbg ${VIRTUAL_ENV_FG} ${VIRTUAL_ENV_BG})${VIRTUAL_ENV_SYMBOL}$environ $(fg ${VIRTUAL_ENV_BG})"
+  fi
 }
 
-PROMPT_COMMAND=powerline_prompt_command
+##############################################
+# (3)
+##############################################
+function build_scm_prompt() {
+  scm_prompt_vars
 
+  if [[ "${SCM_NONE_CHAR}" == "${SCM_CHAR}" ]]; then
+    return
+  fi
+  if [[ "${SCM_GIT_CHAR}" != "${SCM_CHAR}" ]]; then
+    return
+  fi
+
+  local fg_local
+  if [[ "${SCM_DIRTY}" -eq 3 ]]; then
+    fg_local=${SCM_STAGED_FG}
+  elif [[ "${SCM_DIRTY}" -eq 2 ]]; then
+    fg_local=${SCM_UNSTAGED_FG}
+  elif [[ "${SCM_DIRTY}" -eq 1 ]]; then
+    fg_local=${SCM_DIRTY_FG}
+  else
+    fg_local=${SCM_CLEAN_FG}
+  fi
+
+  ahead=$(sed -nr "s@.*( ${SCM_GIT_AHEAD_CHAR}[0-9]+).*@\\1@pg" <<<${SCM_BRANCH})
+  behind=$(sed -nr "s@.*( ${SCM_GIT_BEHIND_CHAR}[0-9]+).*@\\1@pg" <<<${SCM_BRANCH})
+  without_remote=$(
+    sed -r "s@(.*)( ${SCM_GIT_BEHIND_CHAR}[0-9]+)(.*)@\\1\\3@g" <<<${SCM_BRANCH} |
+      sed -r "s@(.*)( ${SCM_GIT_AHEAD_CHAR}[0-9]+)(.*)@\\1\\3@g"
+  )
+
+  local fg_behind=${SCM_CLEAN_FG}
+  if [[ ${behind} != "" ]]; then
+    fg_behind=${SCM_BEHIND_FG}
+  fi
+
+  local fg_ahead=${SCM_CLEAN_FG}
+  if [[ ${ahead} != "" ]]; then
+    fg_ahead=${SCM_AHEAD_FG}
+  fi
+
+  SCM_PREFIX="$(fgbg ${fg_behind} ${SCM_BG})$(git_remote_logo)${behind} $(fg 0)$(fg ${fg_ahead})${ahead} $(fgbg ${fg_local} ${SCM_BG})${SCM_CHAR}${without_remote}${SCM_STATE}"
+  echo "$(separator ${SCM_BG}) ${bold_white}${SCM_PREFIX} $(fg ${SCM_BG})"
+}
+
+##############################################
+# (4)
+##############################################
+function build_cwd_prompt() {
+  echo "$(separator ${CWD_BG}) $(fgbg ${CWD_FG} ${CWD_BG})$(short_cwd)$(fg ${CWD_BG})"
+}
+
+##############################################
+# (5)
+# Arguments:
+#   $1: last_status_code (ex: 127)
+##############################################
+function build_status_prompt() {
+  if [[ "$1" -ne 0 ]]; then
+    echo "$(separator ${STATUS_BG}) $(fgbg ${STATUS_FG} ${STATUS_BG})${1} $(fg ${STATUS_BG})"
+  fi
+}
+
+##############################################
+# Last
+##############################################
+function tail() {
+  echo "$(separator 0)${normal} "
+}
+
+##############################################
+# Main
+##############################################
+function main() {
+  err_code="$?"
+  PS1="$(build_shell_prompt)$(build_virtualenv_prompt)$(build_scm_prompt)$(build_cwd_prompt)$(build_status_prompt $err_code)$(tail)"
+}
+
+PROMPT_COMMAND=main
