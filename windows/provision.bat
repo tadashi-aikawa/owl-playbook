61a5ab605508c4bbb1b253ca442f77391e8d39ff
tadashi-aikawa <syou.maman@gmail.com>
2021-04-11 22:45:07 +0900

    [IDEA] 2021.1のバージョンに対応

--- windows/provision.bat
+++ windows/provision.bat
@@ -13,7 +13,7 @@ goto :tmp
 
 call :******************** IntelliJ IDEA
 
-set IDEA_DIR=IntelliJIdea2020.3
+set IDEA_DIR=IntelliJIdea2021.1
 
 set IDEA_ORIGIN_CONFIG_DIR=%COMMON_MNT%\IntelliJIdea\config
 set IDEA_CONFIG_DIR=%ROAMING%\JetBrains\%IDEA_DIR%
