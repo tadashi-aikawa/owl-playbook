d8b8cf8b59f05930fca3374f7d8a46f30bdfc0c8
tadashi-aikawa <syou.maman@gmail.com>
2021-05-26 12:15:08 +0900

    [WSL] Distribution名を変更

--- windows/ubuntu/provision.ps1
+++ windows/ubuntu/provision.ps1
@@ -1,13 +1,13 @@
 # 冪等性はないので注意
 # 実行後に編集が必要なファイルもある
-cp $home\.gitconfig \\wsl$\Ubuntu\tmp\
+cp $home\.gitconfig \\wsl$\Ubuntu-20.04\tmp\
 wsl -- mv /tmp/.gitconfig ~/
 # root/browserなどpathの変更が必要
-cp $home\.gowlconfig \\wsl$\Ubuntu\tmp
+cp $home\.gowlconfig \\wsl$\Ubuntu-20.04\tmp
 wsl -- mv /tmp/.gowlconfig ~/
 
 # pathの変更が必要かも..
-cp -r $home\.ssh \\wsl$\Ubuntu\tmp\
+cp -r $home\.ssh \\wsl$\Ubuntu-20.04\tmp\
 wsl -- rm -rf ~/.ssh
 wsl -- mv /tmp/.ssh ~/
 wsl -- chmod 600 ~/.ssh/*
