2932d38a91f80e02fa9ebba89c82f8927cec641d
tadashi-aikawa <syou.maman@gmail.com>
2022-01-18 20:39:54 +0900

    [IDEA] 2021.3.1の起動設定

--- mnt/windows/power-shell/Microsoft.PowerShell_profile.ps1
+++ mnt/windows/power-shell/Microsoft.PowerShell_profile.ps1
@@ -168,7 +168,7 @@ function owl() {
 # Java (IDEA用)
 #-----------------------------------------------------
 
-$env:JAVA_HOME = $env:LOCALAPPDATA + "\JetBrains\Toolbox\apps\IDEA-U\ch-0\213.5744.223\jbr"
+$env:JAVA_HOME = $env:LOCALAPPDATA + "\JetBrains\Toolbox\apps\IDEA-U\ch-0\213.6461.79\jbr"
 
 #-----------------------------------------------------
 # Golang
@@ -180,7 +180,7 @@ $env:GO111MODULE = "on"
 # Execution PATHs
 #-----------------------------------------------------
 
-$env:PATH += ";" + $env:LOCALAPPDATA + "\JetBrains\Toolbox\apps\IDEA-U\ch-0\213.5744.223\bin"
+$env:PATH += ";" + $env:LOCALAPPDATA + "\JetBrains\Toolbox\apps\IDEA-U\ch-0\213.6461.79\bin"
 $env:PATH += ";" + $env:USERPROFILE + "\git\bitbucket.org\ntj-developer\diamant\target\release"
 
 
