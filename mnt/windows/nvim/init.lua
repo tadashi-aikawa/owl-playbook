28a33c6bf17f7622c9b7f67c71353621971aecec
tadashi-aikawa <syou.maman@gmail.com>
2023-03-27 01:57:15 +0900

    [Neovim] hunkをresetするキーを追加

--- mnt/windows/nvim/init.lua
+++ mnt/windows/nvim/init.lua
@@ -182,6 +182,7 @@ key('n', '<C-j>e', ':Telescope frecency<CR>', {silent = true, noremap = true})
 -- gitsigns
 key('n', '<C-j>d', ':Gitsigns preview_hunk<CR>', {silent = true, noremap = true})
 key('n', '<C-j>D', ':Gitsigns diffthis<CR>', {silent = true, noremap = true})
+key('n', '<C-j><C-u>', ':Gitsigns reset_hunk<CR>', {silent = true, noremap = true})
 key('n', '<Space>j', ':Gitsigns next_hunk<CR>', {silent = true, noremap = true})
 key('n', '<Space>k', ':Gitsigns prev_hunk<CR>', {silent = true, noremap = true})
 -- ReplaceWithRegister
