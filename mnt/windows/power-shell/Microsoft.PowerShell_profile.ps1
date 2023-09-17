d5a27db9508b906cdcae317333e807215a223976
tadashi-aikawa <syou.maman@gmail.com>
2023-09-17 12:31:41 +0900

    [git-graph] gllコマンドの表示数を15に変更

--- mnt/windows/power-shell/Microsoft.PowerShell_profile.ps1
+++ mnt/windows/power-shell/Microsoft.PowerShell_profile.ps1
@@ -140,7 +140,7 @@ function gbm()  { git branch -l | rg -v '^\* ' | % { $_ -replace " ", "" } | fzf
 
 # git log
 function gls()   { git-graph -n 30 -s round }
-function gll()   { git-graph -n 60 -s round --format "%h %d %s%n 💿%ad 👤<%ae>%n　"}
+function gll()   { git-graph -n 15 -s round --format "%h %d %s%n 💿%ad 👤<%ae>%n　"}
 function glll()  { git log --graph --all --date=format:'%Y-%m-%d %H:%M' --pretty=format:'%C(auto)%d%Creset\ %C(yellow)%h%Creset %C(magenta)%ae%Creset %C(cyan)%ad%Creset%n%C(white bold)%w(80)%s%Creset%n%b' }
 function glls()  { git log --graph --all --date=format:'%Y-%m-%d %H:%M' --pretty=format:'%C(auto)%d%Creset\ %C(yellow)%h%Creset %C(magenta)%ae%Creset %C(cyan)%ad%Creset%n%C(white bold)%w(80)%s%Creset%n%b' -10}
 
