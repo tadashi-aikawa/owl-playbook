04d9ea63c66470b65f8bcfac8450c5d922680003
tadashi-aikawa <syou.maman@gmail.com>
2023-09-23 10:33:36 +0900

    WSLに関する設定削除

--- windows/ubuntu/provision.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-# Install ansible
-# https://docs.ansible.com/ansible/latest/user_guide/windows_faq.html#can-ansible-run-on-windows
-sudo apt-get update
-sudo apt-get install python3-pip libffi-dev libssl-dev -y
-pip3 install ansible pywinrm
