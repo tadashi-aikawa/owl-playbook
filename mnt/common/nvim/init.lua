2076db19d4889768a5bddc5006942948fc937c25
tadashi-aikawa <syou.maman@gmail.com>
2023-10-03 13:35:57 +0900

    Gitsignsのreset_hunkのキーバインドを<C-j>uに変更

--- mnt/common/nvim/init.lua
+++ mnt/common/nvim/init.lua
@@ -263,7 +263,7 @@ require("lazy").setup({
     keys = {
       { '<C-j>d', ':Gitsigns preview_hunk<CR>' },
       { '<C-j>D', ':Gitsigns diffthis<CR>' },
-      { '<C-j><C-u>', ':Gitsigns reset_hunk<CR>' },
+      { '<C-j>u', ':Gitsigns reset_hunk<CR>' },
       { '<Space>s', ':Gitsigns stage_hunk<CR>' },
       { '<Space>j', ':Gitsigns next_hunk<CR>' },
       { '<Space>k', ':Gitsigns prev_hunk<CR>' },
