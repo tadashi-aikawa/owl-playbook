a016773e0db00cefbb9971af25e1c17c22acd803
tadashi-aikawa <syou.maman@gmail.com>
2023-10-06 08:38:29 +0900

    peek.nvimを追加

--- mnt/common/nvim/init.lua
+++ mnt/common/nvim/init.lua
@@ -331,7 +331,21 @@ require("lazy").setup({
         { '<M-p>', ':MarkdownPreviewToggle<CR>' }
       },
       build = function() vim.fn["mkdp#util#install"]() end,
+  },
+  {
+      "toppair/peek.nvim",
+       event = { "BufRead", "BufNewFile" },
+       build = "deno task --quiet build:fast",
+       config = function()
+         require("peek").setup{
+           theme = 'light',
+           app = 'browser'
+         }
+         vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+         vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+       end
   }
+  -- Markdown preview
 })
 
 -----------------------------------------------------
