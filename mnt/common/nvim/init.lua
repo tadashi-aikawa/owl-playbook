9d2b7128833dce0167ed1557672e1ff01ab6fc79
tadashi-aikawa <syou.maman@gmail.com>
2023-10-11 17:14:15 +0900

    lualineにオリジナルカスタマイズを適応

--- mnt/common/nvim/init.lua
+++ mnt/common/nvim/init.lua
@@ -175,7 +175,43 @@ local neovim_plugins = {
     dependencies = { 'nvim-web-devicons', opt = true },
     event = {'BufNewFile', 'BufRead'},
     options = { theme = 'gruvbox-material' },
-    config = 'require("lualine").setup()'
+    config = function()
+      local lualine = require('lualine')
+      local config = {
+        options = {
+          component_separators = {},
+          section_separators = {},
+        },
+        sections = {
+          lualine_a = {'filename'},
+          lualine_b = {'branch'},
+          lualine_c = {
+            "'%='",
+            {
+              'diff',
+              symbols = {added = ' ', modified = ' ', removed = ' '},
+              separator = "  |  ",
+            },
+            {
+              'diagnostics',
+              symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '},
+            },
+          },
+          lualine_x = {'encoding', 'fileformat'},
+          lualine_y = {'filetype','searchcount'},
+          lualine_z = {},
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = {},
+        },
+      }
+      lualine.setup(config)
+    end
   },
 
   -- マルチカーソル
