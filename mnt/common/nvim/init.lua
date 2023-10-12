1a4b8915e2baf3b5442866735b254624e0452f59
tadashi-aikawa <syou.maman@gmail.com>
2023-10-12 11:26:28 +0900

    Telescopeのブランチを0.1.xに

--- mnt/common/nvim/init.lua
+++ mnt/common/nvim/init.lua
@@ -235,7 +235,7 @@ local neovim_plugins = {
   -- Fuzzy finder
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.1',
+    branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim', "nvim-telescope/telescope-frecency.nvim", "kkharji/sqlite.lua" },
     keys = {
       { '<C-j>f', ':Telescope find_files find_command=rg,--files,--hidden,--glob,!*.git <CR>' },
