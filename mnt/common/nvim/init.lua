4676247d02156daf8c24dca8a181b3bf581d4bce
tadashi-aikawa <syou.maman@gmail.com>
2023-11-19 23:50:40 +0900

    [Neovim] mini.nvimのツールをいくつか追加

--- mnt/common/nvim/init.lua
+++ mnt/common/nvim/init.lua
@@ -297,8 +297,9 @@ local neovim_plugins = {
       'nvim-tree/nvim-web-devicons'
     },
     keys = {
-      { '<M-w>',  ':NvimTreeToggle<CR>' },
-      { '<C-j>w', ':NvimTreeFindFile<CR>' }
+      -- Use mini.files instead
+      -- { '<M-w>',  ':NvimTreeToggle<CR>' },
+      -- { '<C-j>w', ':NvimTreeFindFile<CR>' }
     },
     config = function()
       require("nvim-tree").setup {
@@ -464,7 +465,75 @@ local neovim_plugins = {
       { '<M-p>', ':MarkdownPreviewToggle<CR>' }
     },
     build = function() vim.fn["mkdp#util#install"]() end,
-  }
+  },
+
+  -- mini.nvim
+  {
+    'echasnovski/mini.nvim',
+    version = false,
+    event = { 'BufNewFile', 'BufRead' },
+    keys = {
+      { '<C-j>w', ':lua MiniFiles.open()<CR>' }
+    },
+    config = function()
+      local miniclue = require('mini.clue')
+      require('mini.clue').setup({
+        window = {
+          delay = 500
+        },
+        triggers = {
+          -- Leader triggers
+          { mode = 'n', keys = '<Leader>' },
+          { mode = 'x', keys = '<Leader>' },
+
+          -- Built-in completion
+          { mode = 'i', keys = '<C-x>' },
+
+          -- `g` key
+          { mode = 'n', keys = 'g' },
+          { mode = 'x', keys = 'g' },
+
+          -- Marks
+          { mode = 'n', keys = "'" },
+          { mode = 'n', keys = '`' },
+          { mode = 'x', keys = "'" },
+          { mode = 'x', keys = '`' },
+
+          -- Registers
+          { mode = 'n', keys = '"' },
+          { mode = 'x', keys = '"' },
+          { mode = 'i', keys = '<C-r>' },
+          { mode = 'c', keys = '<C-r>' },
+
+          -- Window commands
+          { mode = 'n', keys = '<C-w>' },
+
+          -- `z` key
+          { mode = 'n', keys = 'z' },
+          { mode = 'x', keys = 'z' },
+        },
+
+        clues = {
+          -- Enhance this by adding descriptions for <Leader> mapping groups
+          miniclue.gen_clues.builtin_completion(),
+          miniclue.gen_clues.g(),
+          miniclue.gen_clues.marks(),
+          miniclue.gen_clues.registers(),
+          miniclue.gen_clues.windows(),
+          miniclue.gen_clues.z(),
+        },
+      })
+
+      require('mini.files').setup({
+        mappings = {
+          go_in_plus = '<CR>',
+        },
+      })
+
+      require('mini.indentscope').setup()
+    end
+  },
+
 }
 
 require('lazy').setup(
