850b86d6efc12129be05502e40c5cc7d7c7b2b02
tadashi-aikawa <syou.maman@gmail.com>
2023-12-10 20:16:58 +0900

    [Neovim] 不要なlazygit.nvimの削除

--- mnt/common/nvim/init.lua
+++ mnt/common/nvim/init.lua
@@ -525,14 +525,6 @@ local neovim_plugins = {
     end
   },
 
-  -- LazyGit
-  {
-    "kdheepak/lazygit.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-  },
-
   -- VSCode like
   {
     'neoclide/coc.nvim',
