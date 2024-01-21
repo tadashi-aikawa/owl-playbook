a8b372fad4bd52e5d0f092afc2ac8c6ec1dbc692
tadashi-aikawa <syou.maman@gmail.com>
2024-01-22 01:04:12 +0900

    [Leap] 移動先候補を複数ウィンドウにする

--- mnt/common/nvim/init.lua
+++ mnt/common/nvim/init.lua
@@ -76,8 +76,11 @@ local common_plugins = {
     'ggandor/leap.nvim',
     config = function()
       key('n', 's', function()
-        local current_window = vim.fn.win_getid()
-        require('leap').leap { target_windows = { current_window } }
+        local focusable_windows = vim.tbl_filter(
+          function(win) return vim.api.nvim_win_get_config(win).focusable end,
+          vim.api.nvim_tabpage_list_wins(0)
+        )
+        require('leap').leap { target_windows = focusable_windows }
       end)
     end
   },
