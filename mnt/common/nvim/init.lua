fd044e1e325a8767b5d8d847e81a1018d7301492
tadashi-aikawa <syou.maman@gmail.com>
2024-01-21 13:36:02 +0900

    [Neovim] 警告の対応

--- mnt/common/nvim/init.lua
+++ mnt/common/nvim/init.lua
@@ -359,7 +359,7 @@ local neovim_plugins = {
   {
     'mg979/vim-visual-multi',
     init = function()
-      t = {}
+      local t = {}
       t["Find Under"] = "<C-k>"
       t["Find Subword Under"] = "<C-k>"
       vim.g.VM_maps = t
