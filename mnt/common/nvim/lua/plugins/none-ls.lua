871adedae98adc3dd4376c97e0e8c608da94f63c
tadashi-aikawa <syou.maman@gmail.com>
2024-01-28 22:08:43 +0900

    [LSP] goの自動インポート対応

--- mnt/common/nvim/lua/plugins/none-ls.lua
+++ mnt/common/nvim/lua/plugins/none-ls.lua
@@ -9,6 +9,7 @@ return {
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.formatting.goimports,
       },
       on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
