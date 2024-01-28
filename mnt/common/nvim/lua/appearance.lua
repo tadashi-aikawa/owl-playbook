3df1e28eb38f8ef249c4e9428940bd7ccc57c452
tadashi-aikawa <syou.maman@gmail.com>
2024-01-29 02:26:37 +0900

    [Neovim] format

--- mnt/common/nvim/lua/appearance.lua
+++ mnt/common/nvim/lua/appearance.lua
@@ -1,7 +1,7 @@
 local u = require("utils")
 
 -- Color scheme
-u.set.background = 'dark'
+u.set.background = "dark"
 
 -- 行番号の表示
 u.set.number = true
@@ -14,17 +14,17 @@ u.set.updatetime = 200
 
 -- Highlight
 vim.api.nvim_set_hl(0, "YankHighlight", { bg = "#885522" })
-vim.api.nvim_set_hl(0, 'Visual', { bg = "#661111" })
+vim.api.nvim_set_hl(0, "Visual", { bg = "#661111" })
 -- vim.api.nvim_set_hl(0, "MatchParen", { fg = "#121212", bg = "#EE9999" })
 -- -- For illuminate.nvim
 -- vim.api.nvim_set_hl(0, "IlluminatedWordText", { fg = 'lightgray', bg = 'darkcyan' })
-vim.api.nvim_set_hl(0, "IlluminatedWordRead", { fg = 'black', bg = "#abab23" })
+vim.api.nvim_set_hl(0, "IlluminatedWordRead", { fg = "black", bg = "#abab23" })
 -- vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { fg = 'lightgray', bg = 'darkred' })
 
 -- Yankした範囲をハイライトさせる
 vim.api.nvim_create_autocmd("TextYankPost", {
   pattern = "*",
   callback = function()
-    vim.highlight.on_yank({ higroup = 'YankHighlight', timeout = 200 })
+    vim.highlight.on_yank({ higroup = "YankHighlight", timeout = 200 })
   end,
 })
