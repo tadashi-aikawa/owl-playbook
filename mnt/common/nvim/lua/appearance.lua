eaea4cc0501f21eeda38c424b4de9ee719f871d7
tadashi-aikawa <syou.maman@gmail.com>
2024-01-29 12:52:27 +0900

    [Illuminate] 書き込みシンボルの色を設定

--- mnt/common/nvim/lua/appearance.lua
+++ mnt/common/nvim/lua/appearance.lua
@@ -27,7 +27,7 @@ vim.api.nvim_set_hl(0, "Visual", { bg = "#661111" })
 -- -- For illuminate.nvim
 -- vim.api.nvim_set_hl(0, "IlluminatedWordText", { fg = 'lightgray', bg = 'darkcyan' })
 vim.api.nvim_set_hl(0, "IlluminatedWordRead", { fg = "black", bg = "#abab23" })
--- vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { fg = 'lightgray', bg = 'darkred' })
+vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { fg = "lightgray", fg = "#121212", bg = "#EE9999" })
 
 -- Yankした範囲をハイライトさせる
 vim.api.nvim_create_autocmd("TextYankPost", {
