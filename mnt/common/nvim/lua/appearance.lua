2aa21953b8f7ce2bbc64a7fcaf150ebaa8c69c99
tadashi-aikawa <syou.maman@gmail.com>
2024-01-29 19:39:06 +0900

    [Neovim] カラースタイルの調整

--- mnt/common/nvim/lua/appearance.lua
+++ mnt/common/nvim/lua/appearance.lua
@@ -14,7 +14,7 @@ vim.api.nvim_set_hl(0, "LineNr", { fg = "#888888" })
 -- Comment
 vim.api.nvim_set_hl(0, "Comment", { fg = "#888888" })
 -- GitGutter
-vim.api.nvim_set_hl(0, "GitGutterAddLineNr", { fg = "#00FF00" })
+vim.api.nvim_set_hl(0, "GitGutterAddLineNr", { fg = "#46cd46" })
 vim.api.nvim_set_hl(0, "GitGutterChangeLineNr", { fg = "#FFFFFF" })
 vim.api.nvim_set_hl(0, "GitGutterDeleteLineNr", { fg = "#FF4545" })
 -- Highlight
@@ -22,8 +22,8 @@ vim.api.nvim_set_hl(0, "YankHighlight", { bg = "#885522" })
 vim.api.nvim_set_hl(0, "Visual", { bg = "#661111" })
 -- vim.api.nvim_set_hl(0, "MatchParen", { fg = "#121212", bg = "#EE9999" })
 -- -- For illuminate.nvim
--- vim.api.nvim_set_hl(0, "IlluminatedWordText", { fg = 'lightgray', bg = 'darkcyan' })
-vim.api.nvim_set_hl(0, "IlluminatedWordRead", { fg = "black", bg = "#abab23" })
+vim.api.nvim_set_hl(0, "IlluminatedWordText", { fg = "black", bg = "#abab23" })
+vim.api.nvim_set_hl(0, "IlluminatedWordRead", { fg = "black", bg = "#23ab23" })
 vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { fg = "lightgray", fg = "#121212", bg = "#EE9999" })
 
 -- Yankした範囲をハイライトさせる
