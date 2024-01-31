e10783be9cab46dbd9e9148d1285d536ca6dcb9e
tadashi-aikawa <syou.maman@gmail.com>
2024-01-31 23:00:19 +0900

    [Illuminate] 不要なfiletypeでは無効化する

--- mnt/common/nvim/lua/plugins/vim-illuminate.lua
+++ mnt/common/nvim/lua/plugins/vim-illuminate.lua
@@ -3,6 +3,11 @@ return {
   config = function()
     require("illuminate").configure({
       delay = 300,
+      filetypes_denylist = {
+        "aerial",
+        "NvimTree",
+        "notify",
+      },
     })
   end,
 }
