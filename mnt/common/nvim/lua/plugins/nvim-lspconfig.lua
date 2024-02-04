3593bae341354b9327bd1766ed838773dbcf4610
tadashi-aikawa <syou.maman@gmail.com>
2024-02-05 01:32:04 +0900

    不要設定を削除

--- mnt/common/nvim/lua/plugins/nvim-lspconfig.lua
+++ mnt/common/nvim/lua/plugins/nvim-lspconfig.lua
@@ -237,32 +237,5 @@ return {
 
     -- -- import最適化
     -- { '<M-o>', ':call CocAction(\'runCommand\', \'editor.action.organizeImport\')<CR>', silent = true },
-
-    -- -- TABでsnippets展開とplaceholder移動
-    -- {
-    --   "<tab>",
-    --   '<Plug>(coc-snippets-expand-jump)',
-    --   mode = "i",
-    --   silent = true,
-    -- },
   end,
 }
-
--- ['<Tab>'] = cmp.mapping(function(fallback)
---   if cmp.visible() then
---     cmp.select_next_item()
---   elseif luasnip.expand_or_jumpable() then
---     luasnip.expand_or_jump()
---   else
---     fallback()
---   end
--- end, { 'i', 's' }),
--- ['<S-Tab>'] = cmp.mapping(function(fallback)
---   if cmp.visible() then
---     cmp.select_prev_item()
---   elseif luasnip.jumpable(-1) then
---     luasnip.jump(-1)
---   else
---     fallback()
---   end
--- end, { 'i', 's' }),
