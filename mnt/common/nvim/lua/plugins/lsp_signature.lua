e6de9bba79aeb79110544c0715a98d4c593b80c3
tadashi-aikawa <syou.maman@gmail.com>
2024-02-04 17:28:19 +0900

    [lsp_signature] 読み込みタイミングを早める (ファイル直接開くと発動しないため)

--- mnt/common/nvim/lua/plugins/lsp_signature.lua
+++ mnt/common/nvim/lua/plugins/lsp_signature.lua
@@ -1,6 +1,6 @@
 return {
   "ray-x/lsp_signature.nvim",
-  event = "VeryLazy",
+  event = { "BufNewFile", "BufRead" },
   opts = {},
   config = function(_, opts)
     require("lsp_signature").setup(opts)
