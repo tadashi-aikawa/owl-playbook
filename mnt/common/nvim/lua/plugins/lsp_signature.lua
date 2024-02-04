c0c6f8d997b099da5b82fc48b8022dfaaa5a2868
tadashi-aikawa <syou.maman@gmail.com>
2024-02-04 19:37:31 +0900

    [lsp_signature] <C-p>で手動実行を追加

--- mnt/common/nvim/lua/plugins/lsp_signature.lua
+++ mnt/common/nvim/lua/plugins/lsp_signature.lua
@@ -1,7 +1,9 @@
 return {
   "ray-x/lsp_signature.nvim",
   event = { "BufNewFile", "BufRead" },
-  opts = {},
+  opts = {
+    toggle_key = "<C-p>",
+  },
   config = function(_, opts)
     require("lsp_signature").setup(opts)
   end,
