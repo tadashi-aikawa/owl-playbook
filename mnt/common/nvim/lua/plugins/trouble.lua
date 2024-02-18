a8c310fb333a2c0d32601f99ec1c7c9bd830ddab
tadashi-aikawa <syou.maman@gmail.com>
2024-02-18 18:48:06 +0900

    [Trouble] 設定のカスタマイズ(自動開閉・宣言を非表示・キーバインド・アイコン変更)

--- mnt/common/nvim/lua/plugins/trouble.lua
+++ mnt/common/nvim/lua/plugins/trouble.lua
@@ -1,13 +1,35 @@
 return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {},
   config = function()
+    require("trouble").setup({
+      action_keys = {
+        jump_close = { "<cr>" },
+        jump = { "<tab>" },
+        toggle_fold = { "l", "h" },
+        cancel = {}, -- ESCには割り当てない
+      },
+      -- 診断結果が存在する場合・しない場合で自動開閉
+      auto_open = true,
+      auto_close = true,
+      -- 呼び出し履歴(lsp_references)では宣言を表示しない
+      include_declaration = { "lsp_implementations", "lsp_definitions" },
+      signs = {
+        error = "",
+        warning = "",
+        hint = "󱩎",
+        information = "",
+        other = "",
+      },
+    })
+
+    local trouble = require("trouble")
+
     vim.keymap.set("n", "<C-j>h", function()
-      require("trouble").open("lsp_references")
+      trouble.open("lsp_references")
     end)
     vim.keymap.set("n", "<Space>w", function()
-      require("trouble").toggle("workspace_diagnostics")
+      trouble.toggle("workspace_diagnostics")
     end)
   end,
 }
