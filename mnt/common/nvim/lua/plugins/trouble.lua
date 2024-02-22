78ca62876814ff58704e0dbc96071465de25ff31
tadashi-aikawa <syou.maman@gmail.com>
2024-02-22 11:55:50 +0900

    [Trouble] 自動開閉をOFF

--- mnt/common/nvim/lua/plugins/trouble.lua
+++ mnt/common/nvim/lua/plugins/trouble.lua
@@ -9,9 +9,6 @@ return {
         toggle_fold = { "l", "h" },
         cancel = {}, -- ESCには割り当てない
       },
-      -- 診断結果が存在する場合・しない場合で自動開閉
-      auto_open = true,
-      auto_close = true,
       -- 呼び出し履歴(lsp_references)では宣言を表示しない
       include_declaration = { "lsp_implementations", "lsp_definitions" },
       signs = {
