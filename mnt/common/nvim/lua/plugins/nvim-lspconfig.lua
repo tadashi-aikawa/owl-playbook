565532ad5ad223d98c5565be58eca74e7deed0fe
tadashi-aikawa <syou.maman@gmail.com>
2024-02-27 13:52:39 +0900

    [LSP] nuxt.config.jsの対応

--- mnt/common/nvim/lua/plugins/nvim-lspconfig.lua
+++ mnt/common/nvim/lua/plugins/nvim-lspconfig.lua
@@ -185,7 +185,7 @@ return {
     lspconfig.volar.setup({
       filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
       capabilities = capabilities,
-      root_dir = util.root_pattern("vite.config.ts", "nuxt.config.ts"),
+      root_dir = util.root_pattern("vite.config.ts", "nuxt.config.ts", "nuxt.config.js"),
       on_new_config = function(new_config, new_root_dir)
         new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
       end,
