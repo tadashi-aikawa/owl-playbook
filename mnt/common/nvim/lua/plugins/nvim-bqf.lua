62fccb73ca9023d2a516be895cf955068a81f4e1
tadashi-aikawa <syou.maman@gmail.com>
2024-02-29 20:37:10 +0900

    [nvim-bqf] Enterで決定した場合はquickfixを閉じる(Tabで保持)

--- mnt/common/nvim/lua/plugins/nvim-bqf.lua
+++ mnt/common/nvim/lua/plugins/nvim-bqf.lua
@@ -3,6 +3,8 @@ return {
   config = function()
     require("bqf").setup({
       func_map = {
+        openc = "<CR>",
+        open = "<Tab>",
         split = "<C-s>",
         vsplit = "<F12>",
       },
