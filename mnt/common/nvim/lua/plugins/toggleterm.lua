96faa86700d080403f9440306c4ed12df86bcd1f
tadashi-aikawa <syou.maman@gmail.com>
2024-03-17 16:16:10 +0900

    [toggleterm] ターミナルジョブモードで`ESC`でノーマルモードに移行できるようにする

--- mnt/common/nvim/lua/plugins/toggleterm.lua
+++ mnt/common/nvim/lua/plugins/toggleterm.lua
@@ -5,5 +5,6 @@ return {
     require("toggleterm").setup({})
     vim.keymap.set("n", "<Space>i", ":ToggleTerm direction=float<CR>", { silent = true })
     vim.keymap.set("n", "<Space>,", ":ToggleTerm size=20<CR>", { silent = true })
+    vim.keymap.set("t", "<ESC>", [[<C-\><C-n>]], { silent = true })
   end,
 }
