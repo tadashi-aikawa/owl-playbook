e9a3339fe15866da3dcf0257e466862b575244d6
tadashi-aikawa <syou.maman@gmail.com>
2024-04-18 14:21:56 +0900

    [Lspsaga] レイアウト微調整

--- mnt/common/nvim/lua/plugins/lspsaga.lua
+++ mnt/common/nvim/lua/plugins/lspsaga.lua
@@ -14,7 +14,7 @@ return {
       finder = {
         max_height = 0.7,
         left_width = 0.3,
-        right_width = 0.6,
+        right_width = 0.7,
         keys = {
           toggle_or_open = "<CR>",
           shuttle = "]w",
