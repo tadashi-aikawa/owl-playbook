a7f575b433ca7c107fee8dc322cfb9f7a3b7edf5
tadashi-aikawa <syou.maman@gmail.com>
2024-04-22 10:04:34 +0900

    [vue-language-server] 確認コマンドを適切にする

--- linux/provision.sh
+++ linux/provision.sh
@@ -251,7 +251,7 @@ npm_install typescript tsc
 npm_install typescript-language-server
 
 # Vue
-npm_install @vue/language-server
+npm_install @vue/language-server vue-language-server
 
 # Svelte
 npm_install svelte-language-server svelteserver
