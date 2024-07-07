1f588f73ebed345de5c58be87e593b376082b264
tadashi-aikawa <syou.maman@gmail.com>
2024-07-07 16:00:04 +0900

    [Trouble] v3対応

--- mnt/common/nvim/lua/plugins/trouble.lua
+++ mnt/common/nvim/lua/plugins/trouble.lua
@@ -1,17 +1,46 @@
 return {
   "folke/trouble.nvim",
+  cmd = "Trouble",
+  keys = {
+    {
+      "<Space>w",
+      "<cmd>Trouble diagnostics toggle<cr>",
+    },
+    {
+      "<C-j>h",
+      "<cmd>Trouble lsp_references<cr>",
+    },
+  },
   config = function()
     require("trouble").setup({
-      action_keys = {
-        jump_close = { "<cr>" },
-        jump = { "o" },
-        toggle_fold = { "l", "h" },
-        cancel = {}, -- ESCには割り当てない
-        open_split = { "<C-s>" },
-        open_vsplit = { "<F12>" },
+      focus = true,
+      auto_refresh = false,
+      keys = {
+        ["<cr>"] = "jump_close",
+        o = "jump",
+        l = "fold_open",
+        h = "fold_close",
+        ["<F12>"] = "jump_vsplit",
+        ["<esc>"] = "inspect",
+        -- XXX: sはバインドしたくないが、fallbackの方法が分からず...
+        s = function()
+          require("flash").jump()
+        end,
       },
-      -- 呼び出し履歴(lsp_references)では宣言を表示しない
-      include_declaration = { "lsp_implementations", "lsp_definitions" },
+      lsp_references = {
+        params = {
+          -- 呼び出し履歴(lsp_references)では宣言を表示しない
+          include_declaration = false,
+        },
+      },
+      modes = {
+        lsp_base = {
+          params = {
+            -- 現在の項目が消えてしまうので...
+            include_current = true,
+          },
+        },
+      },
       signs = {
         error = "",
         warning = "",
@@ -23,17 +52,11 @@ return {
 
     local trouble = require("trouble")
 
-    vim.keymap.set("n", "<C-j>h", function()
-      trouble.open("lsp_references")
-    end)
-    vim.keymap.set("n", "<Space>w", function()
-      trouble.toggle("workspace_diagnostics")
-    end)
     vim.keymap.set("n", "<Space>n", function()
       trouble.next({ skip_groups = true, jump = true })
     end)
     vim.keymap.set("n", "<Space>p", function()
-      trouble.previous({ skip_groups = true, jump = true })
+      trouble.prev({ skip_groups = true, jump = true })
     end)
   end,
 }
