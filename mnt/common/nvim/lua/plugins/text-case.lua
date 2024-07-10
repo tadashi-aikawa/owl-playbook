bc6cc2c58c44dd221b483e053cd3ff054a48fcd4
tadashi-aikawa <syou.maman@gmail.com>
2024-07-10 09:22:40 +0900

    [text-case] 対象をcurrent_wordに変更

--- mnt/common/nvim/lua/plugins/text-case.lua
+++ mnt/common/nvim/lua/plugins/text-case.lua
@@ -6,12 +6,12 @@ return {
     require("telescope").load_extension("textcase")
 
     local opts = { noremap = true, silent = true }
-    vim.keymap.set("n", "gas", "<cmd>:lua require('textcase').operator('to_snake_case')<CR>iW", opts)
-    vim.keymap.set("n", "gal", "<cmd>:lua require('textcase').operator('to_snake_case')<CR>iW", opts)
-    vim.keymap.set("n", "gac", "<cmd>:lua require('textcase').operator('to_camel_case')<CR>iW", opts)
-    vim.keymap.set("n", "gap", "<cmd>:lua require('textcase').operator('to_pascal_case')<CR>iW", opts)
-    vim.keymap.set("n", "gak", "<cmd>:lua require('textcase').operator('to_dash_case')<CR>iW", opts)
-    vim.keymap.set("n", "gau", "<cmd>:lua require('textcase').operator('to_constant_case')<CR>iW", opts)
+    vim.keymap.set("n", "gas", "<cmd>:lua require('textcase').current_word('to_snake_case')<CR>", opts)
+    vim.keymap.set("n", "gal", "<cmd>:lua require('textcase').current_word('to_snake_case')<CR>", opts)
+    vim.keymap.set("n", "gac", "<cmd>:lua require('textcase').current_word('to_camel_case')<CR>", opts)
+    vim.keymap.set("n", "gap", "<cmd>:lua require('textcase').current_word('to_pascal_case')<CR>", opts)
+    vim.keymap.set("n", "gak", "<cmd>:lua require('textcase').current_word('to_dash_case')<CR>", opts)
+    vim.keymap.set("n", "gau", "<cmd>:lua require('textcase').current_word('to_constant_case')<CR>", opts)
   end,
   keys = {
     "ga",
