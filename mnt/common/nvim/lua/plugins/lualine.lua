eaa09200833de6d6833f0cd10adf49805535f6ee
tadashi-aikawa <syou.maman@gmail.com>
2024-07-19 07:55:21 +0900

    [lualine] win_barを表示 (incline同等のことをする)

--- mnt/common/nvim/lua/plugins/lualine.lua
+++ mnt/common/nvim/lua/plugins/lualine.lua
@@ -5,25 +5,84 @@ return {
   options = { theme = "gruvbox-material" },
   config = function()
     local lualine = require("lualine")
+
+    local theme_base = {
+      a = { fg = "#1b1d2b", bg = "#82aaff", gui = "bold" },
+      b = { fg = "#82aaff", bg = "#3b4261" },
+      c = { fg = "#828bb8", bg = "#1e2030" },
+    }
+    local theme_base_active = {
+      a = { fg = "#efef33", bg = "#888888", gui = "bold" },
+      b = { fg = "#82aaff", bg = "#3b4261" },
+      c = { fg = "#828bb8", bg = "#1e2030" },
+    }
+    local custom_theme = {
+      normal = theme_base_active,
+      insert = theme_base_active,
+      visual = theme_base_active,
+      replace = theme_base,
+      command = theme_base,
+      inactive = theme_base,
+    }
+
     local config = {
       options = {
+        theme = custom_theme,
         component_separators = {},
         section_separators = {},
+        disabled_filetypes = {
+          statusline = { "no-neck-pain" },
+          winbar = { "no-neck-pain" },
+        },
+      },
+      winbar = {
+        lualine_a = {},
+        lualine_b = { { "diagnostics", sources = { "nvim_lsp" } } },
+        lualine_c = {},
+        lualine_x = {
+          { "diff", symbols = { added = " ", modified = " ", removed = " " } },
+        },
+        lualine_y = {
+          { "filetype", icon_only = true },
+        },
+        lualine_z = {
+          {
+            "filename",
+            newfile_status = true,
+            symbols = {
+              modified = " ",
+              readonly = "󰌾 ",
+            },
+          },
+        },
+      },
+      inactive_winbar = {
+        lualine_a = {},
+        lualine_b = { { "diagnostics", sources = { "nvim_lsp" } } },
+        lualine_c = {},
+        lualine_x = {
+          { "diff", symbols = { added = " ", modified = " ", removed = " " } },
+        },
+        lualine_y = {
+          { "filetype", icon_only = true },
+        },
+        lualine_z = {
+          {
+            "filename",
+            newfile_status = true,
+            symbols = {
+              modified = " ",
+              readonly = "󰌾 ",
+            },
+          },
+        },
       },
       sections = {
-        lualine_a = { "branch" },
-        lualine_b = { { "filename", path = 1 } },
-        lualine_c = {},
-        lualine_x = { "encoding", "fileformat" },
-        lualine_y = { "filetype", "searchcount" },
-        lualine_z = {},
-      },
-      inactive_sections = {
         lualine_a = {},
         lualine_b = {},
         lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
+        lualine_x = { { "filename", path = 3 } },
+        lualine_y = { "encoding", "fileformat" },
         lualine_z = {},
       },
     }
