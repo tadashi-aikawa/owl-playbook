fa634e910e8a36d8536884f313e29da0444e1e70
tadashi-aikawa <syou.maman@gmail.com>
2024-07-22 07:08:13 +0900

    [nvim-ts-autotag] クローズタグの自動挿入を無効化してスラッシュからの補完にする
    hogeに対して<div>hogeと入力しようとすると<div></div>hogeとなってしまうのを防ぐため

--- mnt/common/nvim/lua/plugins/nvim-ts-autotag.lua
+++ mnt/common/nvim/lua/plugins/nvim-ts-autotag.lua
@@ -1,6 +1,11 @@
 return {
   "windwp/nvim-ts-autotag",
   config = function()
-    require("nvim-ts-autotag").setup()
+    require("nvim-ts-autotag").setup({
+      opts = {
+        enable_close = false,
+        enable_close_on_slash = true,
+      },
+    })
   end,
 }
