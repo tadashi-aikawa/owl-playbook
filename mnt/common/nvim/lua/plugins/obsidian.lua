97f49987d0dfe301fe54854ad529372bab2a2ac7
tadashi-aikawa <syou.maman@gmail.com>
2024-08-26 07:53:58 +0900

    [obsidian.nvim] 設定のコメント削除

--- mnt/common/nvim/lua/plugins/obsidian.lua
+++ mnt/common/nvim/lua/plugins/obsidian.lua
@@ -1,16 +1,8 @@
 return {
   "epwalsh/obsidian.nvim",
-  version = "*", -- recommended, use latest release instead of latest commit
+  version = "*",
   lazy = true,
   ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre path/to/my-vault/*.md",
-  --   "BufNewFile path/to/my-vault/*.md",
-  -- },
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
