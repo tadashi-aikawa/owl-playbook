49e482fabdb8bc1f39e9703f762872aac301dbf1
tadashi-aikawa <syou.maman@gmail.com>
2024-08-26 23:46:50 +0900

    [obsidian.nvim] 前方のwikiリンクを瞬時に開けるようにする

--- mnt/common/nvim/lua/plugins/obsidian.lua
+++ mnt/common/nvim/lua/plugins/obsidian.lua
@@ -7,6 +7,34 @@ return {
     "nvim-lua/plenary.nvim",
   },
   config = function()
+    local forward_seek_gf = function()
+      local line = vim.api.nvim_get_current_line()
+      local row = vim.fn.line(".")
+      local col = vim.fn.col(".")
+
+      local cur = 1
+      while true do
+        local st, ed = string.find(line, "%[%[.-%]%]", cur)
+        if not st then
+          break
+        end
+        if col < st then
+          vim.api.nvim_win_set_cursor(vim.api.nvim_get_current_win(), { row, st + 1 })
+          break
+        end
+        if col <= ed then
+          break
+        end
+        cur = ed + 1
+      end
+
+      if require("obsidian").util.cursor_on_markdown_link(nil, nil, true) then
+        vim.cmd([[ :ObsidianFollowLink ]])
+      else
+        vim.cmd([[ gf ]])
+      end
+    end
+
     require("obsidian").setup({
       workspaces = {
         {
@@ -36,19 +64,21 @@ return {
       -- <C-]>でリンク先を開く
       mappings = {
         ["<C-]>"] = {
-          action = function()
-            return require("obsidian").util.gf_passthrough()
-          end,
-          opts = { noremap = false, expr = true, buffer = true },
+          action = forward_seek_gf,
+          opts = { noremap = false, expr = false, buffer = true },
         },
       },
     })
 
-    vim.keymap.set("n", "<C-j>h", ":ObsidianBacklinks<CR>", { silent = true })
     vim.keymap.set("n", "<M-]>", function()
       vim.cmd([[ vsplit ]])
-      vim.cmd([[ :ObsidianFollowLink ]])
+      forward_seek_gf()
     end, { silent = true })
-    vim.keymap.set("n", "g<C-]>", ":ObsidianFollowLink hsplit<CR>", { silent = true })
+    vim.keymap.set("n", "g<C-]>", function()
+      vim.cmd([[ split ]])
+      forward_seek_gf()
+    end, { silent = true })
+
+    vim.keymap.set("n", "<C-j>h", ":ObsidianBacklinks<CR>", { silent = true })
   end,
 }
