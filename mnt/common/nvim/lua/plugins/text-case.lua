ac4951c99fff83466ac337bfe2126aa8c67696f1
tadashi-aikawa <syou.maman@gmail.com>
2024-10-25 02:22:36 +0900

    perf(neovim): ロード時間のかかるプラグインをlazyにして起動時間を短縮

--- mnt/common/nvim/lua/plugins/text-case.lua
+++ mnt/common/nvim/lua/plugins/text-case.lua
@@ -1,21 +1,13 @@
+local opts = { noremap = true, silent = true }
+
 return {
   "johmsalas/text-case.nvim",
-  dependencies = { "nvim-telescope/telescope.nvim" },
-  config = function()
-    require("textcase").setup({})
-    require("telescope").load_extension("textcase")
-
-    local opts = { noremap = true, silent = true }
-    vim.keymap.set("n", "gas", "<cmd>:lua require('textcase').current_word('to_snake_case')<CR>", opts)
-    vim.keymap.set("n", "gal", "<cmd>:lua require('textcase').current_word('to_snake_case')<CR>", opts)
-    vim.keymap.set("n", "gac", "<cmd>:lua require('textcase').current_word('to_camel_case')<CR>", opts)
-    vim.keymap.set("n", "gap", "<cmd>:lua require('textcase').current_word('to_pascal_case')<CR>", opts)
-    vim.keymap.set("n", "gak", "<cmd>:lua require('textcase').current_word('to_dash_case')<CR>", opts)
-    vim.keymap.set("n", "gau", "<cmd>:lua require('textcase').current_word('to_constant_case')<CR>", opts)
-  end,
   keys = {
-    "ga",
-    { "ga.", "<cmd>TextCaseOpenTelescope<CR>", mode = { "n", "x" }, desc = "Telescope" },
+    { "gas", "<cmd>:lua require('textcase').current_word('to_snake_case')<CR>", opts },
+    { "gal", "<cmd>:lua require('textcase').current_word('to_snake_case')<CR>", opts },
+    { "gac", "<cmd>:lua require('textcase').current_word('to_camel_case')<CR>", opts },
+    { "gap", "<cmd>:lua require('textcase').current_word('to_pascal_case')<CR>", opts },
+    { "gak", "<cmd>:lua require('textcase').current_word('to_dash_case')<CR>", opts },
+    { "gau", "<cmd>:lua require('textcase').current_word('to_constant_case')<CR>", opts },
   },
-  lazy = false,
 }
--- mnt/common/nvim/lua/plugins/treesj.lua
+++ mnt/common/nvim/lua/plugins/treesj.lua
@@ -1,11 +1,25 @@
 return {
   "Wansmer/treesj",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
+  keys = {
+    {
+      "<Space>s",
+      function()
+        require("treesj").split()
+      end,
+      silent = true,
+    },
+    {
+      "<Space><S-j>",
+      function()
+        require("treesj").join()
+      end,
+      silent = true,
+    },
+  },
   config = function()
     require("treesj").setup({
       use_default_keymaps = false,
     })
-    vim.keymap.set("n", "<Space>s", require("treesj").split)
-    vim.keymap.set("n", "<Space><S-j>", require("treesj").join)
   end,
 }
