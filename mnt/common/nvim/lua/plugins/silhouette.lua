cbad60bb10b9a16cdf3bcd98d25b67731d4c8476
tadashi-aikawa <syou.maman@gmail.com>
2024-10-26 23:46:46 +0900

    chore(neovim): silhouette.nvimの設定コメント変更

--- mnt/common/nvim/lua/plugins/silhouette.lua
+++ mnt/common/nvim/lua/plugins/silhouette.lua
@@ -10,7 +10,7 @@ return {
   },
   config = function()
     -- denops.nvimが起動し終わる前に実行されるとエラーになるためwaitを入れる
-    -- マシンスペックや環境によって500(ms)の値を調整する必要あり
+    -- マシンスペックや環境によって1000(ms)の値を調整する必要あり
     vim.defer_fn(function()
       require("silhouette").setup({
         task = {
