b65a487ab3748f88e0706e764d20f6e1c7365cee
tadashi-aikawa <syou.maman@gmail.com>
2024-10-27 16:44:41 +0900

    fix(neovim): nvim-lspconfigでlua_lsが単一luaファイルを開いたときエラーになる

--- mnt/common/nvim/lua/plugins/nvim-lspconfig.lua
+++ mnt/common/nvim/lua/plugins/nvim-lspconfig.lua
@@ -223,9 +223,11 @@ return {
     lspconfig.lua_ls.setup({
       capabilities = capabilities,
       on_init = function(client)
-        local path = client.workspace_folders[1].name
-        if vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc") then
-          return
+        if client.workspace_folders then
+          local path = client.workspace_folders[1].name
+          if vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc") then
+            return
+          end
         end
 
         client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
