97737050e16f622ddc0aa32125416bf6b384e2cb
tadashi-aikawa <syou.maman@gmail.com>
2024-11-13 22:42:29 +0900

    feat(oil): 開いているディレクトリパスをtoggletermのターミナルでカレントディレクトリとして開く

--- mnt/common/nvim/lua/plugins/toggleterm.lua
+++ mnt/common/nvim/lua/plugins/toggleterm.lua
@@ -5,6 +5,12 @@ return {
     require("toggleterm").setup({})
     vim.keymap.set("n", "<Space>i", ":ToggleTerm direction=float<CR>", { silent = true })
     vim.keymap.set("n", "<Space>,", ":ToggleTerm size=20<CR>", { silent = true })
+    vim.keymap.set("n", "<Space>t", function()
+      local curdir = require("oil").get_current_dir()
+      if curdir then
+        vim.cmd("ToggleTerm dir=" .. curdir)
+      end
+    end, { silent = true })
     vim.keymap.set("t", "<ESC>", [[<C-\><C-n>]], { silent = true })
   end,
 }
