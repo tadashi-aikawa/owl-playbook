09af7f974b1eb434ba96772c0ca64517dd99a7f0
tadashi-aikawa <syou.maman@gmail.com>
2024-11-15 17:54:16 +0900

    feat(neovim): <Space>_ 初期状態じゃないタスクリストを初期状態に戻すキーバインド追加

--- mnt/common/nvim/lua/keybind.lua
+++ mnt/common/nvim/lua/keybind.lua
@@ -34,7 +34,10 @@ end, { silent = true })
 vim.keymap.set("n", "<Space>@", function()
   vim.cmd("normal ysiW`")
 end, { silent = true })
+-- Marpの強調タスクを取り消しに変更
 vim.keymap.set("n", "<Space>-", ":s/*/\\~/g<CR>", { silent = true })
+-- ghostwriter.nvimのタスク状態を初期化
+vim.keymap.set("n", "<Space>_", ":s/\\v- \\[.\\] (.+) `.+`/- [ ] \\1/<CR><Cmd>nohlsearch<CR>", { silent = true })
 -- カレントウィンドウのファイル相対パスをコピー
 vim.keymap.set("n", "<Space>y", function()
   local relative_path = vim.fn.expand("%:~:.")
