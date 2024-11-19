46766ed063080dea2bdb5865158eb41dce8aa27b
tadashi-aikawa <syou.maman@gmail.com>
2024-11-20 02:31:27 +0900

    Revert "feat(obsidian.nvim): Minervaをworkspaceに追加"
    This reverts commit ba4a92e72179b42f1b21c92cf2879c350e81407f.

--- mnt/common/nvim/lua/plugins/obsidian.lua
+++ mnt/common/nvim/lua/plugins/obsidian.lua
@@ -63,14 +63,6 @@ return {
             notes_subdir = "notes",
           },
         },
-        {
-          name = "work",
-          path = "~/work/minerva",
-          ---@diagnostic disable-next-line: missing-fields
-          overrides = {
-            notes_subdir = "Notes",
-          },
-        },
       },
 
       new_notes_location = "notes_subdir",
