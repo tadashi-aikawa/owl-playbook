0ca23fbdec978fe8190ade0518fb315b697317c4
tadashi-aikawa <syou.maman@gmail.com>
2024-12-04 20:37:36 +0900

    feat(nvim-highlight-colors): Markdownのときはカラーを表示しない

--- mnt/common/nvim/lua/plugins/nvim-highlight-colors.lua
+++ mnt/common/nvim/lua/plugins/nvim-highlight-colors.lua
@@ -1,10 +1,23 @@
 return {
   "brenoprata10/nvim-highlight-colors",
+  ft = {
+    "css",
+    "scss",
+    "html",
+    "vue",
+    "tsx",
+    "svelte",
+    "json",
+    "lua",
+    "typescript",
+    "javascript",
+  },
   opts = {
     render = "virtual",
     virtual_symbol = "",
     virtual_symbol_position = "eol",
     virtual_symbol_suffix = "",
     enable_tailwind = true,
+    exclude_filetypes = { "markdown" },
   },
 }
