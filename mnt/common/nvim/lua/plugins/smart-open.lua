8caffb99cfb652958811c15664d966445be361bc
tadashi-aikawa <syou.maman@gmail.com>
2024-12-04 22:21:16 +0900

    feat(smart-open)!: Windowsのサポートをやめる

--- mnt/common/nvim/lua/plugins/smart-open.lua
+++ mnt/common/nvim/lua/plugins/smart-open.lua
@@ -1,16 +1,9 @@
-local u = require("utils")
-
 return {
   "danielfalk/smart-open.nvim",
   dependencies = {
     { "kkharji/sqlite.lua" },
     { "nvim-telescope/telescope-fzy-native.nvim" },
   },
-  config = function()
-    if u.is_windows then
-      local home = os.getenv("USERPROFILE")
-      vim.g.sqlite_clib_path = home .. "/lib/sqlite3.dll"
-    end
-  end,
   lazy = true,
+  opts = {},
 }
