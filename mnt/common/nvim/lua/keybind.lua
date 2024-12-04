6c89ab29a0f671373be7f4f632e8ae55fdb82f27
tadashi-aikawa <syou.maman@gmail.com>
2024-12-04 19:27:51 +0900

    perf(markdown-toggle): Markdownファイル確定時まで読み込みを遅延する

--- mnt/common/nvim/lua/keybind.lua
+++ mnt/common/nvim/lua/keybind.lua
@@ -56,3 +56,31 @@ vim.keymap.set("n", "<Space>y", function()
   vim.fn.setreg("+", relative_path)
   vim.notify("Copy: " .. relative_path)
 end, { silent = true })
+
+-- Markdownファイルだけに発生するプラグイン間連携の特殊処理
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "markdown-toggle.nvim keymaps",
+  pattern = { "markdown", "markdown.mdx" },
+  callback = function(args)
+    local opts = { silent = true, noremap = true, buffer = args.buf }
+    local toggle = require("markdown-toggle")
+
+    -- F12はCtrl+Enter
+    vim.keymap.set({ "n", "v" }, "<F12>", function()
+      toggle.checkbox()
+      local cline = vim.api.nvim_get_current_line()
+      if string.find(cline, "- %[x%] .+ ``") then
+        vim.cmd("SilhouettePushTimer")
+      end
+    end, opts)
+    vim.keymap.set({ "i" }, "<F12>", function()
+      vim.api.nvim_command("stopinsert")
+      vim.schedule(function()
+        toggle.checkbox()
+      end)
+      vim.schedule(function()
+        vim.api.nvim_command("startinsert")
+      end)
+    end, opts)
+  end,
+})
--- mnt/common/nvim/lua/plugins/markdown-toggle.lua
+++ mnt/common/nvim/lua/plugins/markdown-toggle.lua
@@ -1,38 +1,9 @@
 return {
   "roodolv/markdown-toggle.nvim",
-  config = function()
-    -- optsに設定を書くとCtrl+Enter実行後にSilhouettePushTimerが実行されないのでconfigに集約
-    require("markdown-toggle").setup({
-      cycle_box_table = true,
-      box_table = { "x" },
-      list_before_box = true,
-    })
-
-    vim.api.nvim_create_autocmd("FileType", {
-      desc = "markdown-toggle.nvim keymaps",
-      pattern = { "markdown", "markdown.mdx" },
-      callback = function(args)
-        local opts = { silent = true, noremap = true, buffer = args.buf }
-        local toggle = require("markdown-toggle")
-
-        -- F12はCtrl+Enter
-        vim.keymap.set({ "n", "v" }, "<F12>", function()
-          toggle.checkbox()
-          local cline = vim.api.nvim_get_current_line()
-          if string.find(cline, "- %[x%] .+ ``") then
-            vim.cmd("SilhouettePushTimer")
-          end
-        end, opts)
-        vim.keymap.set({ "i" }, "<F12>", function()
-          vim.api.nvim_command("stopinsert")
-          vim.schedule(function()
-            toggle.checkbox()
-          end)
-          vim.schedule(function()
-            vim.api.nvim_command("startinsert")
-          end)
-        end, opts)
-      end,
-    })
-  end,
+  ft = { "markdown", "markdown.mdx" },
+  opts = {
+    cycle_box_table = true,
+    box_table = { "x" },
+    list_before_box = true,
+  },
 }
