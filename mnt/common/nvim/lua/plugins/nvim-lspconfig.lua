36d5ee251dcaab03e2108d08f05b52c3baeb68a4
tadashi-aikawa <syou.maman@gmail.com>
2024-12-23 00:54:30 +0900

    feat(nvim-lspconfig): JavaScriptファイルでもVolar LSPを使う(package.jsonがある場合のみ)

--- mnt/common/nvim/lua/plugins/nvim-lspconfig.lua
+++ mnt/common/nvim/lua/plugins/nvim-lspconfig.lua
@@ -217,7 +217,17 @@ return {
         vue = {
           hybridMode = false,
         },
+        typescript = {
+          tsdk = os.getenv("HOME")
+            .. "/.local/share/mise/installs/npm-typescript/latest/lib/node_modules/typescript/lib/",
+        },
       },
+      on_new_config = function(new_config, new_root_dir)
+        local lib_path = vim.fs.find("node_modules/typescript/lib", { path = new_root_dir, upward = true })[1]
+        if lib_path then
+          new_config.init_options.typescript.tsdk = lib_path
+        end
+      end,
     })
 
     lspconfig.tailwindcss.setup({
