a37ebe8945ad3378074979d87cb1c4f653982a3c
tadashi-aikawa <syou.maman@gmail.com>
2025-01-30 22:23:12 +0900

    refactor(neovim): LuaSnip ファイル内容の簡略化とフォーマット無視による可読性向上

--- mnt/common/nvim/luasnippets/markdown.lua
+++ mnt/common/nvim/luasnippets/markdown.lua
@@ -1,24 +1,14 @@
-local ls = require("luasnip")
-local s = ls.snippet
-local i = ls.insert_node
-local d = ls.dynamic_node
-local t = ls.text_node
-local sn = ls.snippet_node
+---@diagnostic disable: undefined-global
 
 local function today()
   return sn(nil, { i(1, os.date("%Y%m%d")) })
 end
 
-ls.add_snippets("markdown", {
+-- stylua: ignore
+return {
   s("_mtg", {
     t("- [ ] "),
-    i(1, "10"),
-    t(":"),
-    i(2, "00"),
-    t(" [[MTG_"),
-    d(4, today),
-    t("_"),
-    i(3, "MTG名"),
-    t("]]"),
+    i(1, "10"), t(":"), i(2, "00"),
+    t(" [[MTG_"), d(3, today), t("_"), i(4, "MTG名"), t("]]"),
   }),
-})
+}
