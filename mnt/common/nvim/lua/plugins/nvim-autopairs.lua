f7aaf99ee7ca258b080780e2865223a495029c15
tadashi-aikawa <syou.maman@gmail.com>
2025-02-05 16:03:00 +0900

    feat(neovim): nvim-autopairsで({[を自動で閉じないようにする

--- mnt/common/nvim/lua/plugins/nvim-autopairs.lua
+++ mnt/common/nvim/lua/plugins/nvim-autopairs.lua
@@ -8,6 +8,17 @@ return {
       disable_filetype = { "TelescopePrompt", "vim" },
     })
 
+    -- 閉じ括弧補完を無効化
+    p.get_rule("("):with_pair(function()
+      return false
+    end)
+    p.get_rule("{"):with_pair(function()
+      return false
+    end)
+    p.get_rule("["):with_pair(function()
+      return false
+    end)
+
     -- クォーテーションは無効化
     p.remove_rule('"')
     p.remove_rule("'")
