7828b5cb3cc2babbfbb787553437ebb061a59248
tadashi-aikawa <syou.maman@gmail.com>
2025-02-11 17:44:08 +0900

    fix(conform): Denoプロジェクトの場合は常にLSPのフォーマットを使う

--- mnt/common/nvim/lua/plugins/conform.lua
+++ mnt/common/nvim/lua/plugins/conform.lua
@@ -2,7 +2,18 @@ return {
   "stevearc/conform.nvim",
   event = "VeryLazy",
   opts = function()
-    local web_formatter = { "biome-check", "prettierd", stop_after_first = true }
+    local function is_deno_project()
+      local cwd = vim.fn.getcwd()
+      return vim.fn.filereadable(cwd .. "/deno.json") == 1 or vim.fn.filereadable(cwd .. "/deno.jsonc") == 1
+    end
+
+    local web_formatter = function()
+      if is_deno_project() then
+        -- Denoプロジェクトの場合はLSP(denols)のフォーマットを使う (fallback)
+        return {}
+      end
+      return { "biome-check", "prettierd", stop_after_first = true }
+    end
     return {
       formatters_by_ft = {
         lua = { "stylua" },
@@ -28,6 +39,7 @@ return {
       },
       format_on_save = {
         timeout_ms = 1500,
+        -- conformで定義したformatterが存在しないならLSPのフォーマッターを使う
         lsp_format = "fallback",
       },
     }
