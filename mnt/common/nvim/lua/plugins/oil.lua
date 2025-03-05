2398718743a32650c4645832b6e84b084e741649
tadashi-aikawa <syou.maman@gmail.com>
2025-03-05 21:06:36 +0900

    feat(oil): gyでエントリの絶対パスをコピー

--- mnt/common/nvim/lua/plugins/oil.lua
+++ mnt/common/nvim/lua/plugins/oil.lua
@@ -8,6 +8,7 @@ return {
     keymaps = {
       ["<C-s>"] = "actions.select_split",
       ["<F12>"] = "actions.select_vsplit",
+      ["gy"] = "actions.copy_entry_path",
       -- ["<C-o>"] = { "actions.send_to_qflist", opts = { action = "a", only_matching_search = true } },
     },
     view_options = {
