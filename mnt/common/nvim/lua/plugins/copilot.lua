db4398f4bba656c676014842108f68f5c1f390cc
tadashi-aikawa <syou.maman@gmail.com>
2025-03-13 08:51:03 +0900

    feat(copilot/copilotChat): copilot.luaとcopilotChatを追加

---
+++ mnt/common/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,13 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  opts = {
+    suggestion = {
+      auto_trigger = true,
+      keymap = {
+        accept = "<tab>",
+      },
+    },
+  },
+}
---
+++ mnt/common/nvim/lua/plugins/copilotChat.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {},
+  },
+}
