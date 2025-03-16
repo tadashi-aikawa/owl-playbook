f1f7f9a05d65743434602fa22e0de04e58f6ead0
tadashi-aikawa <syou.maman@gmail.com>
2025-03-17 08:18:41 +0900

    feat(text-case): gaではなく<Space>aを<Leader>にする
    CodeCompanionとkeymapが被るため

--- mnt/common/nvim/lua/plugins/text-case.lua
+++ mnt/common/nvim/lua/plugins/text-case.lua
@@ -3,12 +3,12 @@ return {
   keys = function()
     local opts = { noremap = true, silent = true }
     return {
-      { "gas", "<cmd>:lua require('textcase').current_word('to_snake_case')<CR>", opts },
-      { "gal", "<cmd>:lua require('textcase').current_word('to_snake_case')<CR>", opts },
-      { "gac", "<cmd>:lua require('textcase').current_word('to_camel_case')<CR>", opts },
-      { "gap", "<cmd>:lua require('textcase').current_word('to_pascal_case')<CR>", opts },
-      { "gak", "<cmd>:lua require('textcase').current_word('to_dash_case')<CR>", opts },
-      { "gau", "<cmd>:lua require('textcase').current_word('to_constant_case')<CR>", opts },
+      { "<Space>as", "<cmd>:lua require('textcase').current_word('to_snake_case')<CR>", opts },
+      { "<Space>al", "<cmd>:lua require('textcase').current_word('to_snake_case')<CR>", opts },
+      { "<Space>ac", "<cmd>:lua require('textcase').current_word('to_camel_case')<CR>", opts },
+      { "<Space>ap", "<cmd>:lua require('textcase').current_word('to_pascal_case')<CR>", opts },
+      { "<Space>ak", "<cmd>:lua require('textcase').current_word('to_dash_case')<CR>", opts },
+      { "<Space>au", "<cmd>:lua require('textcase').current_word('to_constant_case')<CR>", opts },
     }
   end,
   cmd = {
