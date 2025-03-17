de6ed4640b5619e6fd1382f335a9a9291e6b1dca
tadashi-aikawa <syou.maman@gmail.com>
2025-03-18 08:53:28 +0900

    feat(CodeCompanion): 設定を色々と最新化

--- mnt/common/nvim/lua/plugins/code-companion.lua
+++ mnt/common/nvim/lua/plugins/code-companion.lua
@@ -2,8 +2,21 @@ return {
   "olimorris/codecompanion.nvim",
   keys = {
     {
+      "<Space>cf",
+      ":CodeCompanion<CR>",
+      mode = { "n", "v" },
+      silent = true,
+    },
+    {
       "<Space>cc",
       ":CodeCompanionChat<CR>",
+      mode = { "n", "v" },
+      silent = true,
+    },
+    {
+      "<Space>ca",
+      ":CodeCompanionAction<CR>",
+      mode = { "n", "v" },
       silent = true,
     },
   },
@@ -12,6 +25,37 @@ return {
     "nvim-treesitter/nvim-treesitter",
   },
   opts = function(_, opts)
-    return vim.tbl_deep_extend("force", opts, {}, require("envs.code-companion").opts)
+    -- 環境に依存しない設定
+    local base_opts = {
+      opts = {
+        language = "Japanese",
+      },
+      display = {
+        chat = {
+          auto_scroll = false,
+          show_header_separator = true,
+        },
+      },
+      strategies = {
+        chat = {
+          roles = {
+            llm = function(adapter)
+              return " CodeCompanion (" .. adapter.formatted_name .. ")"
+            end,
+            user = " Me",
+          },
+          keymaps = {
+            send = {
+              modes = { n = "<F12>", i = "<F12>" }, -- Ctrl+Enter
+            },
+          },
+        },
+      },
+    }
+    -- 環境ごとに切り分けたい設定
+    local env_opts = require("envs.code-companion").opts
+
+    -- デフォルト設定 -> 環境に依存しない設定 -> 環境に依存する設定 の順にマージ
+    return vim.tbl_deep_extend("force", opts, base_opts, env_opts)
   end,
 }
