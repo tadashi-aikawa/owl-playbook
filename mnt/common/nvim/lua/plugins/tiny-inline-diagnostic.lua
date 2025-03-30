601d79945e682accd1c35d24a3781c227bd0cb74
tadashi-aikawa <syou.maman@gmail.com>
2025-03-30 23:52:47 +0900

    refactor(tiny-inline-diagnostic): Neovim 0.11で不要になった設定を削除

--- mnt/common/nvim/lua/plugins/tiny-inline-diagnostic.lua
+++ mnt/common/nvim/lua/plugins/tiny-inline-diagnostic.lua
@@ -2,19 +2,13 @@ return {
   "rachartier/tiny-inline-diagnostic.nvim",
   event = "LspAttach",
   priority = 1000,
-  config = function()
-    require("tiny-inline-diagnostic").setup({
-      options = {
-        show_source = true,
-        multilines = {
-          enabled = true,
-          always_show = false,
-        },
+  opts = {
+    options = {
+      show_source = true,
+      multilines = {
+        enabled = true,
+        always_show = false,
       },
-    })
-    vim.diagnostic.config({
-      -- native LSPのインライン表示を無効にする
-      virtual_text = false,
-    })
-  end,
+    },
+  },
 }
