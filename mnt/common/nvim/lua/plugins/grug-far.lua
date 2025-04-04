3008d4fe477c99f6e2a2e1fe52e085bf2123878a
tadashi-aikawa <syou.maman@gmail.com>
2025-04-04 19:52:18 +0900

    feat(grug-far): キーバインド変更

--- mnt/common/nvim/lua/plugins/grug-far.lua
+++ mnt/common/nvim/lua/plugins/grug-far.lua
@@ -1,7 +1,17 @@
 return {
   "MagicDuck/grug-far.nvim",
   keys = {
-    { "gr", ":GrugFar<CR>", mode = { "n", "v" }, silent = true },
+    { "gru", ":GrugFar<CR>", mode = { "n", "v" }, silent = true },
   },
-  opts = {},
+  opts = {
+    keymaps = {
+      close = { n = "<localleader>q" },
+      refresh = { n = "<localleader>r" },
+      previewLocation = { n = "<localleader>d" },
+      abort = { n = "<localleader>u" },
+      historyOpen = { n = "<localleader>e" },
+      historyAdd = { n = "<localleader>a" },
+      swapEngine = { n = "<localleader>_" },
+    },
+  },
 }
