5369854655fad5d2cf283f40ad23ab75779415e7
tadashi-aikawa <syou.maman@gmail.com>
2025-04-05 22:00:01 +0900

    feat(neovim): ウィンドウクローズ系のキーバインドを追加 (<Space>m <Space><Space>m <Space><Space>n)
    左小指の負担を和らげるため

--- mnt/common/nvim/lua/keymaps.lua
+++ mnt/common/nvim/lua/keymaps.lua
@@ -5,6 +5,11 @@
 
 vim.g.maplocalleader = ","
 
+-- ウィンドウのクローズ
+vim.keymap.set("n", "<Space>m", ":q<CR>", { silent = true })
+vim.keymap.set("n", "<Space><Space>m", ":qa<CR>", { silent = true })
+vim.keymap.set("n", "<Space><Space>n", ":qa!<CR>", { silent = true })
+
 -- Hippie completion
 vim.keymap.set("i", "<F18>", "<C-x><C-p>") -- Ubuntu(WSL)ではS-F6がF18となるため
 
