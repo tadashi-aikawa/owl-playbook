8e812007e18761e159edc203335ef53eed8d48b6
tadashi-aikawa <syou.maman@gmail.com>
2025-04-12 10:43:48 +0900

    feat(gitsigns): <Space>Dをdiff表示に割り当て

--- mnt/common/nvim/lua/plugins/gitsigns.lua
+++ mnt/common/nvim/lua/plugins/gitsigns.lua
@@ -3,7 +3,13 @@ return {
   event = { "BufNewFile", "BufRead" },
   keys = {
     { "<Space>d", ":Gitsigns preview_hunk<CR>", silent = true },
-    { "<C-j>D", ":Gitsigns diffthis<CR>", silent = true },
+    {
+      "<Space>D",
+      function()
+        require("gitsigns").diffthis()
+      end,
+      silent = true,
+    },
     { "<Space>u", ":Gitsigns reset_hunk<CR>", silent = true },
     { "<Space>j", ":Gitsigns next_hunk<CR>", silent = true },
     { "<Space>k", ":Gitsigns prev_hunk<CR>", silent = true },
