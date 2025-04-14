bdc5bb7c6de29c7e3a4cb89708ec0028076184bd
tadashi-aikawa <syou.maman@gmail.com>
2025-04-14 19:24:28 +0900

    feat(snippets): フロントマターにcreatedを追加

--- mnt/common/nvim/luasnippets/markdown.lua
+++ mnt/common/nvim/luasnippets/markdown.lua
@@ -3,6 +3,10 @@
 local function today()
   return sn(nil, { i(1, os.date("%Y%m%d")) })
 end
+local function today_with_hypens()
+  return sn(nil, { i(1, os.date("%Y-%m-%d")) })
+end
+
 
 -- stylua: ignore
 return {
@@ -14,5 +18,46 @@ return {
   s("_activity", {
     t("- [ ] "),
     t("[["), d(1, today), t("_"), i(2, "作業名"), t("]]"),
+  }),
+  s("_fm", {
+    t({
+      "---",
+      "created: "
+    }),
+    d(1, today_with_hypens),
+    t({
+      "",
+      "refer: "
+    }),
+    i(0, ""),
+    t({
+      "",
+      "---",
+      ""
+    }),
+  }),
+  s("_fmp", {
+    t({
+      "---",
+      "created: "
+    }),
+    d(1, today_with_hypens),
+    t({
+      "",
+      "participants:",
+      "  - tadashi-aikawa",
+      "  - ",
+    }),
+    i(2),
+    t({
+      "",
+      "refer: "
+    }),
+    i(0, ""),
+    t({
+      "",
+      "---",
+      ""
+    }),
   }),
 }
